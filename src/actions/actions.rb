b79fee7dda9b99884b9b9b2fd9cfa939139092e2
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-08-24 16:41:07 -0500

    Test driven devlopment de cambio de dirección de serpiente

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -1,22 +1,42 @@
 module Actions
     def self.move_snake(state)
-        next_direction = state.next_direction
+        next_direction = state.curr_direction
         next_position = calc_next_position(state) 
         #Verificar que la siguiente casilla sea valida
-        if position_is_valid?(state, next_position)
+        if position_is_food?(state, next_position)
+            grow_snake_to(state, curr_position)
+            elsif position_is_valid?(state, next_position) 
             move_snake_to(state, next_position)
         else
             end_game(state)
         end
         
-    state
+        state
+    end
+
+    def self.change_direction(state, direction)
+        if next_direction_is_valid?(state, direction)
+            state.curr_direction = direction
+         else puts "Invalid direction"
+        end
+        state
     end
 
     private 
 
+    def self.position_is_food?(state, next_position)
+        state.food.row == next_position.row && state.food.col == next_position.col
+    end
+
+    def self.grow_snake_to(state, next_position)
+        new_positions = [curr_direction] + state.snake.positions
+        state.snake.positions = new_positions
+        state
+    end
+
     def self.calc_next_position(state)
         curr_position = state.snake.positions.first
-        case state.next_direction
+        case state.curr_direction
         when Model::Direction::UP
             #decrementar fila
             return Model::Coord.new(
@@ -54,8 +74,8 @@ module Actions
     end
 
     def self.move_snake_to(state, next_position)
-        new_positions = [next_position] + state.snake.positions[0 ... -1]
-        state.snake.positions = new_positions
+        next_positions = [next_position] + state.snake.positions[0 ... -1]
+        state.snake.positions = next_positions
         state
     end
 
@@ -63,4 +83,20 @@ module Actions
         state.game_finished = true
         state 
     end
-end
+
+    def self.next_direction_is_valid?(state, direction)
+        case state.curr_direction
+        when Model::Direction::UP
+            return true if direction != Model::Direction::DOWN
+        when Model::Direction::DOWN
+            return true if direction != Model::Direction::UP
+        when Model::Direction::RIGHT
+            return true if direction != Model::Direction::LEFT
+        when Model::Direction::LEFT
+            return true if direction !+ Model::Direction::RIGHT
+        end
+
+        return false
+    end
+
+end 
